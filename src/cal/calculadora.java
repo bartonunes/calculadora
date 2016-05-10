e914886512ee1f1b998785f2cbea54702315ea2e
bartonunes <bartonunes@hotmail.com>
2016-05-10 20:52:50 -0300

    calculadora

---
+++ src/cal/calculadora.java
@@ -0,0 +1,28 @@
+package cal;
+
+public class calculadora {
+
+	public double n1,n2;
+	
+	public  double soma(double n1,double n2) {
+		return n1+n2;
+		
+	}
+	
+	public  double subtrair(double n1,double n2) {
+		return n1-n2;
+		
+	}
+	
+	public  double multiplicar(double n1,double n2) {
+		return n1*n2;
+		
+	}
+	
+	public  double dividir(double n1,double n2) {
+		
+		return n1/n2;
+		
+	}
+	
+}
---
+++ src/cal/calcular.java
@@ -0,0 +1,14 @@
+package cal;
+
+public class calcular {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		calculadora c = new calculadora();
+		
+		
+		System.out.println(c.soma(4,2));
+	}
+
+}
