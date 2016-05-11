8bd164bc3c7ee3fa6b43598d460f446229d2f886
bartonunes <bartonunes@hotmail.com>
2016-05-10 21:23:39 -0300

    versao 2.0.0

--- src/cal/calculadora.java
+++ src/cal/calculadora.java
@@ -1,13 +1,13 @@
 package cal;
 
 
-/*versão 1.0.0  estavel data 10/05/2016*/
+/*versao 2.0.0  estavel data 10/05/2016*/
 public class calculadora {
 
 	public double n1,n2;
 	
 	public  double soma(double n1,double n2) {
-		return n1+n2;
+		return n1*n2;
 		
 	}
 	
